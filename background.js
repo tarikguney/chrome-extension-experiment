74a4de7fc30893347d11c60240da79d31308f714
Tarik Guney <atarikguney@gmail.com>
2022-01-28 01:28:05 -0700

    Initial extension project

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = 'red';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+});
