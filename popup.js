e838bd2645512b60bacb4dbdeb53ed173b441cac
Tarik Guney <atarikguney@gmail.com>
2022-02-02 01:52:22 -0700

    Popup click sending a message to background service, which executes backgroun change script

--- popup.js
+++ popup.js
@@ -1,25 +1,6 @@
-// Initialize button with user's preferred color
-let searchGoogleButton = document.getElementById("searchGoogle");
+let changeBackgroundColorButton = document.getElementById("changeBackgroundColor");
 
- function searchGoogle(url) {
-    //if (url.includes("https://www.google.com")) {
-        let searchText = document.getElementsByName("q")[0];
-        searchText.value = "I have felt lucky!";
-
-        let searchButton = document.getElementsByName("btnK")[0];
-        searchButton.click();
-
-        console.info("I have done some work for you!");
-  /*  } else {
-        console.error("You are calling me from a wrong place!")
-    }*/
-}
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-searchGoogleButton.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files:["/content-scripts/feeling-lucky.js"]
-    });
+changeBackgroundColorButton.addEventListener("click", () => {
+    console.log("Button is clicked!");
+    chrome.runtime.sendMessage({backgroundColor: "red"});
 });
